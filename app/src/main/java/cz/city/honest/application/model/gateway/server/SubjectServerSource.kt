241891b6dbebd96b4f363c33092f890aaa830c51
michal.keder <michal.keder@etnetera.cz>
2021-07-02 11:41:15 +0200

    @Query params changed to @QueryMap

--- app/src/main/java/cz/city/honest/application/model/gateway/server/SubjectServerSource.kt
+++ app/src/main/java/cz/city/honest/application/model/gateway/server/SubjectServerSource.kt
@@ -1,22 +1,17 @@
 package cz.city.honest.application.model.gateway.server
 
-import cz.city.honest.application.model.dto.Position
 import cz.city.honest.application.model.dto.WatchedSubject
 import cz.city.honest.application.model.gateway.SubjectGateway
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.QueryMap
 
 
 interface SubjectServerSource : SubjectGateway {
 
     @GET(SubjectEndpointsUrl.SUBJECT_IN_AREA)
-    fun getSubjectsInArea(@Query("longitude")longitude:Double,@Query("latitude") latitude: Double): Observable<GetSubjectsResponse>
+    fun getSubjectsInArea(@QueryMap request: Map<String, String>): Observable<GetSubjectsResponse>
 }
-
-data class GetSubjectsRequest(
-    val userPosition: Position
-)
 
 data class GetSubjectsResponse(
     val subjects: MutableMap<String, List<WatchedSubject>>
--- app/src/main/java/cz/city/honest/application/model/service/subject/SubjectService.kt
+++ app/src/main/java/cz/city/honest/application/model/service/subject/SubjectService.kt
@@ -1,16 +1,12 @@
 package cz.city.honest.application.model.service.subject
 
 import cz.city.honest.application.model.dto.*
-import cz.city.honest.application.model.gateway.server.GetSubjectsRequest
-import cz.city.honest.application.model.gateway.server.GetSubjectsResponse
 import cz.city.honest.application.model.gateway.server.SubjectServerSource
 import cz.city.honest.application.model.repository.subject.SubjectRepository
-import cz.city.honest.application.model.service.update.PublicUpdatable
 import cz.city.honest.application.model.service.RepositoryProvider
-
+import cz.city.honest.application.model.service.update.PublicUpdatable
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
 import java.time.LocalDate
 
 
@@ -20,43 +16,50 @@ class SubjectService(
     val positionProvider: PositionProvider
 ) : PublicUpdatable {
 
-    fun getSubjects():  Flowable<out WatchedSubject> =
+    fun getSubjects(): Flowable<out WatchedSubject> =
         Flowable.fromIterable(subjectRepositories.values)
             .map { addFakeSubject() }
-            //.flatMap { it.get() }
+    //.flatMap { it.get() }
 
-    private fun addFakeSubject()  =
-            ExchangePoint(
-                "21", LocalDate.now(), HonestyStatus.DISHONEST,
-                Position(14.423777, 50.084344),
-                mutableListOf(),
-                ExchangeRate(
-                    "", Watched(LocalDate.now(), LocalDate.now()), mutableSetOf(
-                        Rate("cze", ExchangeRateValues(1.0)),
-                        Rate("eur", ExchangeRateValues(17.0)),
-                        Rate("usd", ExchangeRateValues(25.0)),
-                        Rate("trr", ExchangeRateValues(22.0))
+    private fun addFakeSubject() =
+        ExchangePoint(
+            "21", LocalDate.now(), HonestyStatus.DISHONEST,
+            Position(14.423777, 50.084344),
+            mutableListOf(),
+            ExchangeRate(
+                "", Watched(LocalDate.now(), LocalDate.now()), mutableSetOf(
+                    Rate("cze", ExchangeRateValues(1.0)),
+                    Rate("eur", ExchangeRateValues(17.0)),
+                    Rate("usd", ExchangeRateValues(25.0)),
+                    Rate("trr", ExchangeRateValues(22.0))
 
-                    )
-                ),
-                "aaa".toByteArray()
-            )
+                )
+            ),
+            "aaa".toByteArray()
+        )
 
     override fun update(): Observable<Unit> =
         positionProvider.provide()
             .firstOrError()
-            .flatMapObservable { subjectServerSource.getSubjectsInArea(it.longitude,it.latitude)}
-            .flatMap { Observable.fromIterable(it.subjects.entries)}
-            .flatMap { RepositoryProvider.provide(
-                subjectRepositories,
-                it.key
-            ).insertList(it.value)}
+            .flatMapObservable { subjectServerSource.getSubjectsInArea(toRequest(it)) }
+            .flatMap { Observable.fromIterable(it.subjects.entries) }
+            .flatMap {
+                RepositoryProvider.provide(
+                    subjectRepositories,
+                    it.key
+                ).insertList(it.value)
+            }
             .onErrorComplete()
+
+    private fun toRequest(position: Position) = mapOf(
+        "userPosition.latitude" to position.latitude.toString(),
+        "userPosition.longitude" to position.longitude.toString()
+    )
 
 
 }
 
-interface PositionProvider{
+interface PositionProvider {
 
     fun provide(): Observable<Position>
 
