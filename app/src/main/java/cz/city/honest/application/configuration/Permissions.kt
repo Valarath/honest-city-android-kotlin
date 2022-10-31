f85b4b52a92d54b9388b21f62d0bb153a5bdc832
Michal <valarath@gmail.com>
2022-10-31 17:14:53 +0100

    Missing camera Permission added

--- app/src/main/java/cz/city/honest/application/configuration/Permissions.kt
+++ app/src/main/java/cz/city/honest/application/configuration/Permissions.kt
@@ -11,7 +11,8 @@ class Permissions {
             Manifest.permission.READ_SYNC_SETTINGS,
             Manifest.permission.WRITE_SYNC_SETTINGS,
             "android.permission.AUTHENTICATE_ACCOUNTS",
-            Manifest.permission.GET_ACCOUNTS
+            Manifest.permission.GET_ACCOUNTS,
+            Manifest.permission.CAMERA
         )
     }
 }
